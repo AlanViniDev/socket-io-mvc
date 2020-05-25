d29b3788d613245bc7158373ed400352c99644b6
Alan Vinicius <alan123vinicius@gmail.com>
2020-05-25 01:25:44 -0300

    Add files via upload

--- routes/socketio.route.js
+++ routes/socketio.route.js
@@ -1,14 +1,16 @@
+// requisições bibliotecas
 const express = require('express');
 const router = express.Router();
 const app = express.Router();
 
-// Colocar controller que ainda não foi criado
-
+// requisições controllers
 const view_controller =  require("../control/view.controller");
 const style_controller = require("../control/sytle.controller");
 const js_controller = require("../control/javascript.controller");
 const jq_controller = require("../control/jquery.controller");
 const socketio_controller = require("../control/socketio.controller");
+
+//rotas
 router.get('/index', view_controller.view);
 router.get('/assetsCSS', style_controller.style);
 router.get('/assetsJS', js_controller.javascript);
